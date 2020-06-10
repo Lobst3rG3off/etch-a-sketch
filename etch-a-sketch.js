284a64baed082c4ac3987303f41cbfee6de1fae4
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-10 18:35:15 -0400

    arrow keys make it draw stuff

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,13 +2,11 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-
+const MOVE_AMOUNT = 10;
 
 
 // set up canvas for drawing
 
-// const width = canvas.width;
-// const height = canvas.height;
 
 // make a variable called height and width from the same properties as our canvas
 const {width, height} = canvas;
@@ -23,7 +21,7 @@ let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath();//start the drawing
 ctx.moveTo(x, y);
@@ -34,8 +32,47 @@ ctx.stroke();
 
 //write a draw function
 
+function draw({key}) {
+    console.log(key);
+// start the path
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    // move our x and y values depending on what the user did.
+
+    switch (key) {
+        case 'ArrowUp' :
+            y -= MOVE_AMOUNT;
+            break;
+            case 'ArrowDown' :
+            y += MOVE_AMOUNT;
+            break;
+            case 'ArrowRight' :
+            x += MOVE_AMOUNT;
+            break;
+            case 'ArrowLeft' :
+            x -= MOVE_AMOUNT;
+            break;
+            default:
+                break;
+    }
+
+    ctx.lineTo(x,y);
+    ctx.stroke();
+}
+
+
+
 //write a handler for the keys
+function handleKey(e) {
+ 
+    if (e.key.includes('Arrow')) {
+        e.preventDefault();
+        draw({key: e.key});
+    }
+ 
+}
 
 //clear or shake function
 
 //listen for arrow keys
+window.addEventListener('keydown', handleKey);
